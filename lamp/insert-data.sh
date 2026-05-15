5868083f485bd2c1562cd715df60af2e9eb832b2
Elias <el.ach@gmx.de>
2026-05-15 16:07:20 +0300

    Add command to load article57 data into the database

--- lamp/insert-data.sh
+++ lamp/insert-data.sh
@@ -1,1 +1,2 @@
 docker compose exec mariadb bash -c "mariadb -uroot -pyour_password ygeiopolis  < /var/lib/mysql-files/load.sql"
+docker compose exec mariadb bash -c "mariadb -uroot -pyour_password ygeiopolis  < /var/lib/mysql-files/article57_load.sql"
