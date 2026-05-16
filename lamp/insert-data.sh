41d2b5b6bb21c4b58568d37465f13ec706d9c032
Elias <el.ach@gmx.de>
2026-05-16 18:28:59 +0300

    Remove virtual environment setup from insert-data script

--- lamp/insert-data.sh
+++ lamp/insert-data.sh
@@ -1,15 +1,3 @@
-if [ ! -d ".venv" ]; then
-    python3 -m venv .venv
-    echo "    Created .venv"
-else
-    echo "    .venv already exists, skipping creation"
-fi
-
-source .venv/bin/activate
-
-pip install --quiet --upgrade pip
-pip install --quiet faker mysql-connector-python
-
 docker compose exec mariadb bash -c "mariadb -uroot -pyour_password ygeiopolis  < /var/lib/mysql-files/load.sql"
 docker compose exec mariadb bash -c "mariadb -uroot -pyour_password ygeiopolis  < /var/lib/mysql-files/article57_load.sql"
 python ./init/generate_data.py
